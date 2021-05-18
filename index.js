b9408f7d1490fbdefdc92903c1a29719e892705a
Daniil Chernov <chernov93d@gmail.com>
2021-05-18 07:41:09 +0300

    добавил вью, попробовал пару фишек: - при выборе грузоподъемности свыше 1т (т.е. 3т или 10т) - пропадает последний адрес с Самарой в шаге №4 "Время" - при выборе какого-либо из адресов в шаге №4 - строка подсвечивается зеленым
    Также хочу сделать итоговый заказ с кнопкой в виде отдельного компонента

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 import "./styles.scss";
+import Vue from "vue/dist/vue.esm";
+
+let app = new Vue({
+  el: "#app",
+  data: {
+    weights: ["Не выбрано", "0.5 тонны", "1 тонна", "3 тонны", "10 тонн"],
+    loadings: ["Задняя", "Боковая"],
+    cargos: [
+      "Не выбрано",
+      "Детские товары",
+      "Одежда и обувь",
+      "Стройматериалы",
+    ],
+    hazardClasses: [
+      "Класс 1 Взрывчатые вещества и изделия",
+      "Класс 2 Газы",
+      "Класс 3 Легковоспламеняющиеся жидкости",
+      "Класс 4 Легковоспламеняющиеся твердые вещества, самореактивные вещества и твердые десенсибилизированные взрывчатые вещества",
+      "Класс 5 Окисляющие вещества",
+    ],
+    order: undefined,
+    selectedWeight: undefined,
+    selectedAddress: undefined,
+    addresses: [
+      { address: "г. Москва, Лужники", id: 1 },
+      { address: "г. Екатеринбург, Екатеринбург-арена", id: 2 },
+      { address: "г. Самара, Самара-арена", id: 3 },
+    ],
+  },
+  methods: {
+    isSelected(address) {
+      return this.selectedAddress.id == address.id;
+    },
+    isSelectedWeight(weight, address) {
+      if (address.id == 3) {
+        if (weight.indexOf("3") > -1 || weight.indexOf("10") > -1) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+  },
+  created() {
+    this.order = "Здесь будет Ваш заказ."
+    this.selectedAddress = this.addresses[0];
+    this.selectedWeight = this.weights[0];
+  },
+});
